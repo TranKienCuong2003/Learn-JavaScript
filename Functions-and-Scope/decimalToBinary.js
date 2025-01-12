adb47c421dde8b4ecf00483daf71541284d7d3dc
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-12 20:14:25 +0700

    Add decimalToBinary.js file

---
+++ Functions-and-Scope/decimalToBinary.js
@@ -0,0 +1,7 @@
+function decimalToBinary(decimal) {
+    let binary = decimal.toString(2);
+    console.log("Nhap so thap phan:");
+    console.log("So nhi phan la:", binary);
+}
+let decimal = 10;
+decimalToBinary(decimal);
