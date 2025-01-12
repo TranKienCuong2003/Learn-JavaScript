d932f3918b389f6b5cfe6e0e2cf75b60b8ef2511
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-12 20:13:44 +0700

    Add binaryToDecimal.js file

---
+++ Functions-and-Scope/binaryToDecimal.js
@@ -0,0 +1,7 @@
+function binaryToDecimal(binary) {
+    let decimal = parseInt(binary, 2);
+    console.log("Nhap so nhi phan:");
+    console.log("So thap phan la:", decimal);
+}
+let binary = "1010";
+binaryToDecimal(binary);
